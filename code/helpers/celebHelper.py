dca4ef28f3a1704db2e9a631d5a2ef12df929020
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-27 18:52:34 -0500

    read imageNames along with labels

--- code/helpers/celebHelper.py
+++ code/helpers/celebHelper.py
@@ -1,13 +1,25 @@
-def readImageLabels(filename, columnNumbers=None, skipRows=None):
+import tensorflow as tf
+
+def readNamesImageLabels(filename, columnNumbers=None, skipRows=None):
+	imageNames = []
 	imageLabels = []
 	with open(filename) as file:
 		lines = file.readlines()
 		startRow = 0
+
+		# skip header rows
 		if skipRows is not None:
 			startRow = skipRows - 1
 		for line in lines[startRow:]:
 			cols = line.split()
+
+			# append name of image to list
+			imageNames.append(cols[0])
+
+			# append labels for that image to the list
 			if columnNumbers is not None:
-				imageLabels.append([cols[index] for index in columnNumbers])
+				imageLabels.append([int(cols[index]) for index in range(columnNumbers)])
 			else:
-				imageLabels.append(cols)
+				imageLabels.append([int(cols[index]) for index in range(len(cols))])
+
+	return [imageNames, imageLabels]
